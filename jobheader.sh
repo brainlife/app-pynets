268fec48851553210bf21e13deeaf1d9e004303c
dPys <dpisner@utexas.edu>
2021-06-28 08:29:11 -0500

    Update ppn and add bids structure to input

--- jobheader.sh
+++ jobheader.sh
@@ -5,9 +5,9 @@ if [[ "$(jq -r .mplx config.json)" != "0" ]]; then
 elif [[ "$(jq -r .dwi config.json)" != "0" ]]; then
     walltime=48:00:00
 else
-    walltime=6:00:00
+    walltime=8:00:00
 fi
 
-echo "#PBS -l nodes=1:ppn=1,walltime="$walltime""
+echo "#PBS -l nodes=1:ppn=10,walltime="$walltime""
 echo "#PBS -l vmem=48gb"
 echo "#PBS -N PyNets"
