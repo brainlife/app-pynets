1310293f71c8530ccbd0521d5ae4cf9c765f3c49
dPys <dpisner@utexas.edu>
2021-06-28 19:06:07 -0500

    Update jobheader and id parse

--- jobheader.sh
+++ jobheader.sh
@@ -2,12 +2,15 @@
 
 if [[ "$(jq -r .mplx config.json)" != "0" ]]; then
     walltime=48:00:00
+    ppn=10
 elif [[ "$(jq -r .dwi config.json)" != "0" ]]; then
     walltime=48:00:00
+    ppn=8
 else
     walltime=8:00:00
+    ppn=4
 fi
 
-echo "#PBS -l nodes=1:ppn=1,walltime="$walltime""
+echo "#PBS -l nodes=1:ppn="$ppn",walltime="$walltime""
 echo "#PBS -l vmem=48gb"
 echo "#PBS -N PyNets"
