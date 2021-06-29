511d297d9ced46ebc7fc8843c7860f71e99f1838
dPys <dpisner@utexas.edu>
2021-06-28 21:55:40 -0500

    Update jobheader

--- jobheader.sh
+++ jobheader.sh
@@ -2,13 +2,13 @@
 
 if [[ "$(jq -r .mplx config.json)" != "0" ]]; then
     walltime=48:00:00
-    ppn=1
+    ppn=24
 elif [[ "$(jq -r .dwi config.json)" != "0" ]]; then
     walltime=48:00:00
-    ppn=1
+    ppn=24
 else
     walltime=8:00:00
-    ppn=1
+    ppn=24
 fi
 
 echo "#PBS -l nodes=1:ppn="$ppn",walltime="$walltime""
